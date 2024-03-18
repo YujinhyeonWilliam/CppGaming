907ef1249238ddb9ca28564c80ef1e1c1dd3de20
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-18 19:44:28 +0900

    Integer Theory (Hex, Bin, Dec)

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int hp;
+short mp;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    hp = 0b1100; // 0b: 2진법 (bin)
+    mp = 0xF; // 0x: 8진법 (hex)
+
+    std::cout << mp;
 }
