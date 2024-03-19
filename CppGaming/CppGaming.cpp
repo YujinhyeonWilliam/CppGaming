2c578bde8cbc87d1695f2419e239089cb3a686a9
Jinhyeon <williamjinhyeon@g1play.co.kr>
2024-03-19 12:25:43 +0900

    On halfway of Star Gazing Upwards

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -6,17 +6,38 @@ int main()
 	int N;
 	std::cin >> N;
 
-	for (int i = 0; i < N; i++)
+	int halfN = N / 2;
+
+	for (int i = 0; i < N; i+=2)
 	{
-		for (int j = 0; j < N; j++)
+		if(i < N-1)
+			cout << " ";
+
+		for (int j = 0; j < halfN+i; j++)
 		{
-			if (j < i)
+			if (j < halfN)
 				cout << " ";
 			else
 				cout << "*";
 		}
 		cout << endl;
 	}
+
+	// if(n==3)  halfN  = 1 
+	// 0*    1 (i0)
+	// ***     (i2)
+
+	// if(n==5)  halfN = 2
+	// 00*   2 (i0)
+	// 0***    (i2)
+	// *****   (i4)
+
+	// if(n==7)  halfN = 3
+	// 000*  3 (i0)
+	// 00***   (i2)
+	// 0*****  (i4)
+	// ******* (i6)
+
 }
 
 
