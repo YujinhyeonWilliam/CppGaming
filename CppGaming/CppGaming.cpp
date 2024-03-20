897c084344e0ec4a35b4e6e49c1d9dfbd3a5d9ea
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-20 13:39:19 +0900

    Studied Base of Pointer

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 using namespace std;
+
+void AddHP(int* hp, int value)
+{
+	*hp += value;
+}
+
+int main()
+{
+	int hp = 100;
+	int* ptr = &hp;
+	int** pptr = &ptr;
+
+	int a = 3;
+	//cout << (int)ptr << endl;
+}
+
+
+/*
+
+#include <iostream>
+using namespace std;
 #include "Helper.h"
 #include "Map.h"
 #include "Player.h"
 
+
+[Map and Player Move]
+
 int main()
 {
 	SetCursorOnOff(false);
-	
+
 	while (true)
 	{
 		// ют╥б
@@ -20,3 +44,5 @@ int main()
 		PrintMap2D();
 	}
 }
+
+*/
