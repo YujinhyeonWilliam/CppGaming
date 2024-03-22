ee074735f9cccb0743eb90d5f294be9048cb8263
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-22 14:09:56 +0900

    Pointer Operation

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,19 +1,72 @@
 #include <iostream>
 using namespace std;
 
-void AddHP(int* hp, int value)
+struct StatInfo
 {
-	*hp += value;
+	int hp;
+	int attack;
+	int defence;
+};
+
+StatInfo CreatePlayer()
+{
+	StatInfo info;
+	cout << " 플레이어 생성 " << endl;
+	info.hp = 100;
+	info.attack = 10;
+	info.defence = 2;
+
+	return info;
+}
+
+void CreateMonster(StatInfo* info)
+{
+	info->hp = 40;
+	info->attack = 8;
+	info->defence = 1;
+}
+
+void Battle(StatInfo* player, StatInfo* monster)
+{
+	while (true)
+	{
+	  int damage = player->attack - monster->defence;
+
+	  if (damage < 0)
+		  damage = 0;
+
+	  monster->hp -= damage;
+	  if (monster->hp < 0)
+		  monster->hp = 0;
+
+	  cout << " 몬스터 HP : " << monster->hp << endl;
+
+	  if (monster->hp == 0)
+		  return;
+
+	  damage = monster->attack - player->defence;
+	  if (damage < 0)
+		  damage = 0;
+
+	  cout << " 플레이어 HP : " << player->hp << endl;
+
+	  player->hp -= damage;
+	  if (player->hp < 0)
+		  player->hp = 0;
+
+	  if (player->hp == 0)
+		  return;
+	}
+
 }
 
 int main()
 {
-	int hp = 100;
-	int* ptr = &hp;
-	int** pptr = &ptr;
+	StatInfo player = CreatePlayer();
+	StatInfo monster;
+	CreateMonster(&monster);
 
-	int a = 3;
-	//cout << (int)ptr << endl;
+	Battle(&player, &monster);
 }
 
 
