7b5963fd3216fc5217d2006704eb045031475afd
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-23 08:23:32 +0900

    Singleton Manager with Static and Define Function studied

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -2,67 +2,30 @@
 #include "Player.h"
 using namespace std;
 
-class Pos
+class UserManager
 {
 public:
-	Pos()
+	static UserManager* GetInstance()
 	{
-
+		static UserManager um;
+		return &um;
 	}
 
-	explicit Pos(int b)
-	{
-		x = b;
-		y = b;
-	}
-
-	Pos operator+(const Pos& other)
-	{
-		Pos pos;
-		pos.x = this->x + other.x;
-		pos.y = this->y + other.y;
-		return pos;
-	}
-
-	Pos operator+(const int other)
-	{
-		Pos pos;
-		pos.x = this->x + other;
-		pos.y = this->y + other;
-		return pos;
-	}
-	
-	bool operator==(const Pos& b)
-	{
-		return x == b.x && y == b.y;
-	}
-
-	Pos& operator=(int b)
-	{
-		x = b;
-		y = b;
-		return *this;
-	}
-
-	int x = 0;
-	int y = 0;
+public:
+	void AddUser() { _userCount++; }
+	int GetUserCount() { return _userCount++; }
+private:
+	int _userCount = 0;
 };
 
-Pos operator+(int a, const Pos& b)
-{
-	Pos pos;
-	pos.x = a + b.x;
-	pos.y = a + b.y;
-	return pos;
-}
+#define GET_USER_MANAGER (UserManager::GetInstance())
 
 int main()
 {
-	// C# Knight k = new Knight();
-	// C++ Knight* k = new Knight();
-	Pos pos1;
-	Pos pos2;
-	Pos pos3 = pos1 + pos2;
+	for(int i=0; i<5; i++)
+		GET_USER_MANAGER->AddUser();
+
+	cout << GET_USER_MANAGER->GetUserCount() << endl;
 
 }
 
