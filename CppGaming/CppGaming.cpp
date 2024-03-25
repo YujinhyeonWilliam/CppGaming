2e0b0ee41fe36d6276f9c0af51960bf2652978c8
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-25 15:49:51 +0900

    BFS, DFS

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -3,111 +3,110 @@ using namespace std;
 #include <vector>
 #include <queue>
 
-template<typename T, typename Predicate = less<T>>
-class PriorityQueue
+struct Vertex
 {
-public:
-	// O(logN)
-	void push(const T& data)
-	{
-		// 법칙2: 우선 힙 구조부터 맞춰준다.
-		_heap.push_back(data);
-
-		// 법칙1: 부모 노드는 항상 자식 노드보다 큰 값을 가진다.
-		int now = static_cast<int>(_heap.size()) - 1;
-		while (now > 0)
-		{
-			int next = parent(now);
-
-		  //if (_heap[now] < _heap[next])
-			if(_predicate(_heap[now], _heap[next]))
-				break;
-
-			::swap(_heap[now], _heap[next]);
-			now = next;
-		}
-	}
-
-	// O(logN)
-	void pop()
-	{
-		_heap[0] = _heap.back();
-		_heap.pop_back();
-
-		int now = 0;
-		int heapSize = static_cast<int>(_heap.size());
-		while (true)
-		{
-			int left = leftChild(now);
-			int right = rightChild(now);
-
-			// 리프에 도달한 경우
-			if (left >= heapSize)
-				break;
-
-			int next = now;
-			if (_heap[now] < _heap[left])
-				next = left;
-			
-			//if (right < heapSize && _heap[next] < _heap[right])
-			if (right < heapSize && _predicate(_heap[next], _heap[right]))
-				next = right;
-
-			if (next == now)
-				break;
-			else
-			{
-				::swap(_heap[now], _heap[next]);
-				now = next;
-			}
-		}
-	}
-
-	// O(1)
-	T& top()
-	{
-		return _heap[0];
-	}
-
-	// O(1)
-	bool empty()
-	{
-		return _heap.empty();
-	}
-
-private: 
-	// O(1)
-	int leftChild(int index)
-	{
-		return index * 2 + 1;
-	}
-
-	// O(1)
-	int rightChild(int index)
-	{
-		return index * 2 + 2;
-	}
-
-	// O(1)
-	int parent(int index)
-	{
-		return (index - 1) / 2;
-	}
-
-private:
-	vector<T> _heap;
-	Predicate _predicate;
+	// int data;
 };
+
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+
+//내가 방문한 목록
+vector<bool> visited;
+
+// 발견한
+vector<bool> discovered;
+
+void CreateGraph()
+{
+	vertices.resize(6);
+
+	// 인접 리스트
+	adjacent = vector<vector<int>>(6);
+	adjacent[0] = { 1, 3 };
+	adjacent[1] = { 0, 2, 3 };
+	adjacent[3] = { 4 };
+	adjacent[5] = { 4 };
+
+	// 인접 행렬
+	adjacent = vector<vector<int>>
+	{
+		{0, 1, 0, 1, 0, 0},
+		{1, 0, 1, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+	};
+
+}
+
+
+void BFS(int now)
+{
+	// ex) 누구에 의해서 발견되었는가
+	vector<int> parent(6, -1);
+
+	// ex) 시작점에서 얼만큼 떨어져 있는가?
+	vector<int> dist(6, -1);
+	queue<int> q;
+	q.push(now);
+
+	discovered[now] = true;
+	parent[now] = now;
+	dist[now] = 0;
+
+	while (q.empty() == false)
+	{
+		now = q.front();
+		q.pop();
+
+		// 방문 도장
+		cout << "Visited : " << now << endl;
+
+		// 인접행렬
+		for (int dest = 0; dest < 6; dest++)
+		{
+			if (adjacent[now][dest] == false)
+				continue;
+
+			if (discovered[dest])
+				continue;
+
+			q.push(dest);
+			discovered[dest] = true;
+
+			parent[dest] = now;
+			dist[dest] = dist[now] + 1;
+		}
+
+		// 인접리스트
+		//int size = static_cast<int>(adjacent[now].size());
+		//for(int i=0; i<size; i++)
+		//{
+		//	int dest = adjacent[now][i];
+		//	if (discovered[dest])
+		//		continue;
+
+		//	q.push(dest);
+		//	discovered[dest] = true;
+		//}
+	}
+}
+
+
+void BFSALL()
+{
+	discovered = vector<bool>(6, false);
+	for (int i = 0; i < 6; i++)
+		if (discovered[i] == false)
+			BFS(i);
+}
 
 int main()
 {
-	PriorityQueue<int, greater<int>> pq;
-	pq.push(10);
-	pq.push(40);
-	pq.push(30);
-	pq.push(50);
-	pq.push(20);
-	int value = pq.top();
-	pq.pop();
-	cout << " Top = " << value << endl;
+	CreateGraph();
+	// discovered = vector<bool>(6, false);
+	// BFS(0);
+	BFSALL();
 }
