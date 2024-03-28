2e538185b58ab1a6b6719b0064932340a5253abf
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-28 22:11:49 +0900

    Map

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,19 +1,74 @@
+#include <iostream>
 #include <vector>
 #include <list>
 #include <queue>
-#include "BinarySearchTree.h"
+#include <map>
+using namespace std;
+
+class Player
+{
+public:
+	Player() {}
+	Player(int id) : _id(id) { }
+
+	int _id = 0;
+};
+
+template<typename T, typename U>
+struct Pair
+{
+	T first;
+	U second;
+};
+
+template<typename T, typename U>
+std::pair<T, U> MakePair(T first, U second)
+{
+	return std::pair<T, U>(first, second);
+}
+
 
 int main()
 {
-	BinarySearchTree bst;
-	bst.Insert(20);
-	bst.Insert(30);
-	bst.Insert(10);
-	bst.Insert(25);
-	bst.Insert(26);
-	bst.Insert(40);
-	bst.Insert(50);
+	vector<Player*> v;
+	v.push_back(new Player(200));
+	v.push_back(new Player(400));
+	v.push_back(new Player(300));
+	v.push_back(new Player(500));
+	v.push_back(new Player(100));
 
-	bst.Delete(50);
-	bst.Print();
+	// (Key, Value)
+	map<int, Player*> m;
+
+	// 추가
+	std::pair<int, Player*> p;
+
+	for(Player* player : v)
+	{
+		m.insert(make_pair(player->_id, player));
+	}
+
+	// 찾기
+	auto it = m.find(300); // O(logN)
+	if (it != m.end())
+	{
+		int key = it->first;
+		Player* player = it->second;
+	}
+	else
+	{
+		cout << "데이터 없음" << endl;
+	}
+
+	// 삭제
+	m.erase(it);
+
+	// 순회
+	for (auto it = m.begin(); it != m.end(); it++)
+	{
+		int key = it->first;
+		Player* value = it->second;
+	}
+
+
 }
