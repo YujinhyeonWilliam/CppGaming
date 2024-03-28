1ea8058f55e227e21e2ef3b6f276f1e53f786b72
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-28 13:47:22 +0900

    iterator for list and begin, end function added

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 using namespace std;
-#include "Vector.h"
+#include <vector>
+#include <list>
 #include <queue>
 
 
 int main()
 {
-	{
-		Vector<int> v;
-		v.push_back(10);
-		v.push_back(20);
-		v.push_back(30);
-		Vector<int>::iterator it;
-
-		for (it = v.begin(); it != v.end(); it++)
-		{
-			int data = *it;
-			cout << data << endl;
-		}
-	}
-
 
 }
