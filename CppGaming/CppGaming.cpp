3b48d5b2705ca9649aa70bdbfad0510dfdd3a9d2
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-28 18:18:02 +0900

    ranged-based for

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -5,7 +5,48 @@ using namespace std;
 #include <queue>
 
 
+class Iterator
+{
+public:
+	Iterator() : _data(nullptr) {}
+	Iterator(int* data) : _data(data) {}
+	bool operator==(const Iterator& other)
+	{
+		return _data == other._data;
+	}
+	bool operator!=(const Iterator& other)
+	{
+		return _data != other._data;
+	}
+	void operator++()
+	{
+		_data++;
+	}
+
+	int& operator*()
+	{
+		return *_data;
+	}
+
+public:
+	int* _data;
+};
+class Inventory
+{
+public:
+	using iterator = Iterator;
+
+	iterator begin() { return iterator(&_items[0]); }
+	iterator end()	 { return iterator(&_items[10]); }
+	int _items[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+};
+
 int main()
 {
-
+	Inventory inventory;
+	
+	for (auto item : inventory)
+	{
+		cout << item << endl;
+	}
 }
