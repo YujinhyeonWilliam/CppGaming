ad52ef179d20a2cd1aa56d6a737a8f2a3090d88a
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-28 20:09:11 +0900

    Binary Search Tree

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,40 +1,19 @@
-#include <iostream>
-using namespace std;
 #include <vector>
 #include <list>
 #include <queue>
-
-vector<int> numbers;
-// O(logN)
-void BinarySearch(int N)
-{
-	int left = 0;
-	int right = numbers.size() - 1;
-
-	while (left <= right)
-	{
-		cout << " 탐색 범위 : " << left << "~" << right << endl;
-		int mid = (left + right) / 2;
-		if (N < numbers[mid])
-		{
-			cout << N << "<" << numbers[mid] << endl;
-			right = mid - 1;
-		}
-		else if (N > numbers[mid])
-		{
-			cout << N << ">" << numbers[mid] << endl;
-			left = mid + 1;
-		}
-		else
-		{
-			cout << "찾았음!" << endl;
-			break;
-		}
-	}
-}
+#include "BinarySearchTree.h"
 
 int main()
 {
-	numbers = { 1, 8, 15, 23, 32, 44, 56, 63, 81, 91 };
-	BinarySearch(44);
+	BinarySearchTree bst;
+	bst.Insert(20);
+	bst.Insert(30);
+	bst.Insert(10);
+	bst.Insert(25);
+	bst.Insert(26);
+	bst.Insert(40);
+	bst.Insert(50);
+
+	bst.Delete(50);
+	bst.Print();
 }
