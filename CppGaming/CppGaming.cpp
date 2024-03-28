705c2e16a8fd8d8a1b4d9362294fcad5106945c7
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-28 13:22:06 +0900

    Vector, Iterator customized

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -1,53 +1,24 @@
 #include <iostream>
 using namespace std;
-#include <vector>
+#include "Vector.h"
 #include <queue>
-
-class Functor
-{
-public:
-	void operator()()
-	{
-		cout << "Functor Test" << endl;
-		cout << _value << endl;
-	}
-
-	void operator()(int n)
-	{
-		cout << "Functor Test" << endl;
-		_value += n;
-		cout << _value << endl;
-	}
-
-public:
-	int _value = 0;
-};
-
-class Job
-{
-public:
-	Job() {}
-	virtual ~Job() { }
-};
-
-class MoveJob : Job
-{
-public:
-	MoveJob(int x, int y) : x(x), y(y) {}
-
-	void operator()()
-	{
-		cout << " Player Moved To: x:" << x << ", y :" << y << endl;
-	}
-
-public:
-	int x;
-	int y;
-};
 
 
 int main()
 {
-	MoveJob* job = new MoveJob(10, 20);
-	(*job)();
+	{
+		Vector<int> v;
+		v.push_back(10);
+		v.push_back(20);
+		v.push_back(30);
+		Vector<int>::iterator it;
+
+		for (it = v.begin(); it != v.end(); it++)
+		{
+			int data = *it;
+			cout << data << endl;
+		}
+	}
+
+
 }
---
+++ CppGaming/Vector.h
@@ -0,0 +1,160 @@
+#pragma once
+#include "assert.h"
+#include "stdexcept"
+	// it++;
+	// v.begin();
+	// v.end();
+	// it != v.end();
+	// *it;
+template<typename T>
+class Iterator
+{
+public:
+	Iterator() : _ptr(nullptr){}
+	Iterator(T* ptr) : _ptr(ptr){}
+
+	T& operator*()
+	{
+		return *_ptr;
+	}
+
+	// 전위형 ++it
+	Iterator& operator++()
+	{
+		_ptr++;
+		return *this;
+	}
+
+	// 후위형 it++
+	Iterator operator++(int)
+	{
+		Iterator temp = *this;
+		_ptr++;
+		return temp;
+	}
+
+	Iterator operator+(const int count)
+	{
+		Iterator temp = *this;
+		temp._ptr += count;
+		return temp;
+	}
+
+	bool operator==(const Iterator& other)
+	{
+		return _ptr == other._ptr;
+	}
+
+	bool operator!=(const Iterator& other)
+	{
+		return !(_ptr == other._ptr);
+	}
+
+public:
+	T* _ptr = nullptr;
+};
+
+template<typename T>
+class Vector
+{
+public:
+	using iterator = Iterator<T>;
+
+	iterator begin() { return iterator(&_buffer[0]); }
+	iterator end() { return begin() + _size; }
+
+	explicit Vector()
+	{
+
+	}
+
+	~Vector()
+	{
+		if (_buffer != nullptr)
+			delete[] _buffer;
+	}
+
+	void clear()
+	{
+		if (_buffer != nullptr)
+		{
+			delete[] _buffer;
+			_buffer = new T[_capacity];
+		}
+
+		_size = 0;
+	}
+
+	void push_back(const T& data)
+	{
+		if (_size == _capacity)
+		{
+			// 증설작업 
+			int newCapacity = static_cast<int>(_capacity * 1.5);
+			if (newCapacity == _capacity)
+				newCapacity++;
+
+			reserve(newCapacity);
+		}
+
+		_buffer[_size] = data;
+		_size++;
+	}
+
+	// TODO : 맨 뒤 요소 제거
+	void pop_back()
+	{
+		if (_size > 0)
+		{
+			_buffer[_size - 1] = T();
+			_size--;
+		}
+		else
+			throw std::out_of_range("벡터가 비어있습니다.");
+	}
+
+	void reserve(int capacity)
+	{
+		if (_capacity >= capacity)
+			return;
+
+		_capacity = capacity;
+		T* newData = new T[_capacity];
+
+		for (int i = 0; i < _size; i++)
+			newData[i] = _buffer[i];
+
+		if (_buffer != nullptr)
+			delete[] _buffer;
+
+		_buffer = newData;
+	}
+
+	void resize(int size)
+	{
+		//TODO
+		reserve(size);
+		_size = size;
+
+	}
+
+	T& operator[](int index)
+	{
+		assert(index >= 0 && index < _size);
+		return _buffer[index];
+	}
+
+	T& back()
+	{
+		return _buffer[_size - 1];
+	}
+
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+
+private:
+	T* _buffer = nullptr;
+	int _size = 0;
+	int _capacity = 0;
+};
+
