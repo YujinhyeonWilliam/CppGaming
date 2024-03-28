eba7ce122b37149b580294d3b4e8795bb5c6432a
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-29 08:33:52 +0900

    Sum up

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -3,72 +3,149 @@
 #include <list>
 #include <queue>
 #include <map>
+#include <unordered_map>
 using namespace std;
 
-class Player
+enum class ObjectType
+{
+	Player,
+	Monster,
+	Projectile,
+	Env,
+	NONE
+};
+
+
+class Object
 {
 public:
-	Player() {}
-	Player(int id) : _id(id) { }
-
-	int _id = 0;
+	Object(ObjectType type) : _id(0), _type(type) {}
+	Object(int id) : _id(id), _type(ObjectType::NONE) {}
+	Object(int id, ObjectType type) : _id(id), _type(type) {}
+	virtual ~Object() {}
+	int GetID() const { return _id; }
+	virtual void Update() = 0;
+protected:
+	int _id;
+	ObjectType _type;
 };
 
-template<typename T, typename U>
-struct Pair
+class Player : public Object
 {
-	T first;
-	U second;
+public:
+	Player(ObjectType type) : Object(0){}
+	Player(int id) : Object(id) {}
+	Player(int id, ObjectType type) : Object(id, type) {}
+
+	virtual void Update() override
+	{
+
+	}
+	virtual ~Player() override
+	{
+
+	}
 };
 
-template<typename T, typename U>
-std::pair<T, U> MakePair(T first, U second)
+class Monster : public Object
 {
-	return std::pair<T, U>(first, second);
-}
+public:
+	Monster(ObjectType type) : Object(0) {}
+	Monster(int id) : Object(id) {}
+	Monster(int id, ObjectType type) : Object(id, type) {}
 
+	virtual void Update() override
+	{
+
+	}
+
+	virtual ~Monster() override
+	{
+
+	}
+};
+
+
+class Projectile : public Object
+{
+public:
+	Projectile(ObjectType type) : Object(0) {}
+	Projectile(int id) : Object(id) {}
+	Projectile(int id, ObjectType type) : Object(id, type) {}
+
+	virtual void Update() override
+	{
+
+	}
+
+	virtual ~Projectile() override
+	{
+
+	}
+};
+
+
+class EnV : public Object
+{
+public:
+	EnV(ObjectType type) : Object(0) {}
+	EnV(int id) : Object(id) {}
+	EnV(int id, ObjectType type) : Object(id, type) {}
+
+	virtual void Update() override
+	{
+
+	}
+
+	virtual ~EnV() override
+	{
+
+	}
+};
+
+class Field
+{
+public:
+	static Field* GetInstance()
+	{
+		static Field field;
+		return &field;
+	}
+
+	void Add(Object* player)
+	{
+		_objects.insert(make_pair(player->GetID(), player));
+	}
+
+	void Remove(int id)
+	{
+		_objects.erase(id);
+	}
+
+	void Update() 
+	{
+		for (auto& item : _objects)
+		{
+			item.second->Update();
+		}
+	}
+
+	Object* Get(int id)
+	{
+		auto findIt = _objects.find(id);
+		if (findIt != _objects.end())
+			return findIt->second;
+
+		return nullptr;
+	}
+
+private:
+	unordered_map<int, Object*> _objects;
+
+};
 
 int main()
 {
-	vector<Player*> v;
-	v.push_back(new Player(200));
-	v.push_back(new Player(400));
-	v.push_back(new Player(300));
-	v.push_back(new Player(500));
-	v.push_back(new Player(100));
-
-	// (Key, Value)
-	map<int, Player*> m;
-
-	// 추가
-	std::pair<int, Player*> p;
-
-	for(Player* player : v)
-	{
-		m.insert(make_pair(player->_id, player));
-	}
-
-	// 찾기
-	auto it = m.find(300); // O(logN)
-	if (it != m.end())
-	{
-		int key = it->first;
-		Player* player = it->second;
-	}
-	else
-	{
-		cout << "데이터 없음" << endl;
-	}
-
-	// 삭제
-	m.erase(it);
-
-	// 순회
-	for (auto it = m.begin(); it != m.end(); it++)
-	{
-		int key = it->first;
-		Player* value = it->second;
-	}
-
-
+	Field::GetInstance()->Add(new Player(1));
+	Player* player = dynamic_cast<Player*>(Field::GetInstance()->Get(1));
 }
