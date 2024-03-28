19baff02a824875016c9b9c9c488937b88130ae1
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-28 11:17:20 +0900

    Function Pointer, Function Object

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -3,15 +3,51 @@ using namespace std;
 #include <vector>
 #include <queue>
 
-void Print()
+class Functor
 {
-	cout << "Hello World" << endl;
-}
+public:
+	void operator()()
+	{
+		cout << "Functor Test" << endl;
+		cout << _value << endl;
+	}
+
+	void operator()(int n)
+	{
+		cout << "Functor Test" << endl;
+		_value += n;
+		cout << _value << endl;
+	}
+
+public:
+	int _value = 0;
+};
+
+class Job
+{
+public:
+	Job() {}
+	virtual ~Job() { }
+};
+
+class MoveJob : Job
+{
+public:
+	MoveJob(int x, int y) : x(x), y(y) {}
+
+	void operator()()
+	{
+		cout << " Player Moved To: x:" << x << ", y :" << y << endl;
+	}
+
+public:
+	int x;
+	int y;
+};
+
 
 int main()
 {
-	//typedef void FuncType();
-	using FuncType = void();
-	FuncType* ptr = &Print;
-
+	MoveJob* job = new MoveJob(10, 20);
+	(*job)();
 }
