39b733dc067287db5d6d4b793aab8382aa6cb6c6
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-29 16:58:40 +0900

    String

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -7,42 +7,45 @@
 #include <algorithm>
 using namespace std;
 
-int Partition(vector<int>& v, int left, int right)
-{
-	int pivot = v[left];
-	int low = left + 1;
-	int high = right;
-
-	// O(n)
-	while (low <= high)
-	{
-		while (low <= right && pivot >= v[low])
-			low++;
-
-		while (high >= left + 1 && pivot <= v[high])
-			high--;
-		
-		if (low < high)
-			swap(v[low], v[high]);
-	}
-
-	swap(v[left], v[high]);
-	return high;
-}
-
-// 평균 : O(nlogn), 최악 : O(n^2)
-void QuickSort(vector<int>& v, int left, int right)
-{
-	if (left > right)
-		return;
-
-	int pivot = Partition(v, left, right);
-	QuickSort(v, left, pivot - 1);
-	QuickSort(v, pivot + 1, right);
-}
 
 int main()
 {
-	vector<int> v{ 1, 5, 3, 4, 2 };
-	QuickSort(v, 0, v.size() - 1);
+	// C 스타일 문자열
+	std::string str = "jinhyeon";
+	std::string str2 = "jinhyeon";
+
+	// (1) 비교
+	if (str == str2)
+		cout << "같음" << endl;
+	else
+		cout << "다름" << endl;
+
+	// (2) 복사
+	string str3;
+	str3 = str;
+
+
+	// (3) 추가
+	str.append("12334");
+	str += " 1234";
+
+	// (4) 찾기
+	auto c = str.find("yeo");
+	if (c == string::npos)
+	{
+		cout << " 못찾음 " << endl;
+	}
+
+	// (5) 교체
+	string chatStr = "Shit !!!";
+	string findStr = "Shit";
+	string replaceStr = "****";
+
+	chatStr.replace(chatStr.find(findStr), findStr.length(), replaceStr);
+
+	// (6) 자르기
+	string subStr = str.substr(0, 3);
+
+	// (7) C 스타일 변환
+	const char* nameC = str.c_str();
 }
