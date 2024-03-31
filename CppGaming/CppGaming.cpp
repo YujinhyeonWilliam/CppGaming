1518e6c86b7419a25033c434d7cf8485bf8d90ad
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-31 12:39:58 +0900

    Rvaleu Reference

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -7,26 +7,101 @@
 #include <algorithm>
 using namespace std;
 
+class Pet
+{
+
+};
+
+class Knight
+{
+public:
+	
+	Knight()
+	{
+
+	}
+
+	~Knight()
+	{
+		if (_pet != nullptr)
+			delete _pet;
+	}
+
+	// 복사 생성자
+	Knight(const Knight& knight)
+	{
+		_hp = knight._hp;
+
+
+		// 깊은 복사
+		if (knight._pet)
+			_pet = new Pet(*knight._pet);
+
+		// 얕은 복사
+		//_pet = knight._pet;
+	}
+
+	// 복사 대입 연산자
+	void operator=(const Knight& knight)
+	{
+		_hp = knight._hp;
+		
+
+		// 깊은 복사
+		if (knight._pet)
+			_pet = new Pet(*knight._pet);
+
+		// 얕은 복사
+		//_pet = knight._pet;
+	}
+
+
+	// 이동 생성자
+	Knight(Knight&& knight) noexcept
+	{
+		_hp = knight._hp;
+		_pet = knight._pet;
+		knight._pet = nullptr;
+	}
+
+	// 이동 대입 연산자
+	void operator=(Knight&& knight) noexcept
+	{
+		_hp = knight._hp;
+		_pet = knight._pet;
+		knight._pet = nullptr;
+	}
+
+
+	int _hp = 0;
+	Pet* _pet = nullptr;
+};
+
+void TestKnight_Copy(Knight knight)
+{
+	knight._hp = 100;
+}
+
+void TestKnight_LValueRef(Knight& knight)
+{
+	knight._hp = 100;
+}
+
+
+void TestKnight_ConstValueRef(const Knight& knight)
+{
+	//knight._hp = 100;
+}
+
+void TestKnight_RValueRef(Knight&& knight)
+{
+	//knight._hp = 100;
+}
 
 int main()
 {
-	setlocale(LC_ALL, "");
-	cout << " LC_ALL, " << setlocale(LC_ALL, NULL) << endl;
-
-	// CP949
-	const char* test = "aa유진현";
-	cout << test << endl;
-
-	// UTF-8
-	auto test2 = u8"aa유진현입니다.";
-	setlocale(LC_ALL, "en_US.UTF-8");
-	cout << test2 << endl;
-	setlocale(LC_ALL, "");
-
-	// UTF-16
-	auto test3 = L"aa유진현입니다.";
-	wchar_t ch = L'유';
-	wstring name = L"aaa유진현 입니다.";
-	wcout << name << endl;
-
+	int a = 3;
+	Knight k1;
+	k1._pet = new Pet();
+	Knight k3 = std::move(k1);
 }
