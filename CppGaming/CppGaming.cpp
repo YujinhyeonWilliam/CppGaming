e1373a0bee54ddd5e1a77e935cbf2fa6ef6145ef
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-03-31 18:38:47 +0900

    DP_Enchant

--- CppGaming/CppGaming.cpp
+++ CppGaming/CppGaming.cpp
@@ -8,30 +8,30 @@
 #include <windows.h>
 using namespace std;
 
-int cache[50][50];
+int N = 9;
+int cache[100];
 
-int combination(int n, int r)
+// [+num]부터 시작해서, [+N]까지 가는 경우의 수
+int Enchant(int num)
 {
 	// 기저 사례
-	if (r == 0 || n == r)
+	if (num > N)
+		return 0;
+	if (num == N)
 		return 1;
 
-	// 이미 답을 구한 적이 있으면 바로 반환
-	int& ret = cache[n][r];
+	// 캐시
+	int& ret = cache[num];
 	if (ret != -1)
 		return ret;
 
-	return ret = combination(n - 1, r - 1) + combination(n - 1, r);
+	// 적용
+	return ret = Enchant(num + 1) + Enchant(num + 2) + Enchant(num + 3);
 }
-
 
 int main()
 {
-	::memset(cache, -1, sizeof(cache));
-
-	__int64 start = ::GetTickCount64();
-	int lotto = combination(45, 6);
-	__int64 end = ::GetTickCount64();
-	cout << "Lotto chances = " << lotto << endl;
-	cout << end - start << "ms" << endl;
+	memset(cache, -1, sizeof(cache));
+	int ret = Enchant(0);
+	cout << ret << endl;
 }
